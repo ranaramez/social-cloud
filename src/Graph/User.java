af9dc45519fa04aa502f38720ea114334b29966b
ranaramez <rana.ramez@mashsolvents.com>
2012-05-24 20:58:54 +0200

    changed externalfriends structure

--- src/Graph/User.java
+++ src/Graph/User.java
@@ -2,23 +2,27 @@ package Graph;
 
 import java.util.ArrayList;
 
+import cloud.ExternalFriends;
+
 public class User {
 	String name;
 	int id;
 	double usedMemory;
 	int geolocation;
 	String origin;
-	ArrayList<Integer> externalFriends;
+	ArrayList<ExternalFriends> externalFriends;
 	
 	public User(String name){
 		this.name = name;
 		this.usedMemory = 0.1;
-		this.externalFriends= new ArrayList<Integer>();	
+		this.externalFriends= new ArrayList<ExternalFriends>();	
 	}
 	
 	public User(String name,int geolocation,String origin){
 		this.name=name;
 		this.geolocation=geolocation;
+		this.usedMemory = 0.1;
 		this.origin=origin;
+		this.externalFriends= new ArrayList<ExternalFriends>();
 	}
 }
